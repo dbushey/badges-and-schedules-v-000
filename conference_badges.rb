4edcd46bf40ed2db012374070d1d4bff3d9cefab
Daiane Bushey <daianebushey@gmail.com>
2017-12-29 18:50:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,16 +9,11 @@ def batch_badge_creator(list)
 end
 
 def assign_rooms(list)
-  list.each_with_index do |name, index| 
+  list.each_with_index do |name, index|
     list[name] = index
     "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
-
-
-%w(cat dog wombat).each_with_index { |item, index|
-  hash[item] = index
-}
 
 def printer(list)
   batch_badge_creator
